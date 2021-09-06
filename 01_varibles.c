38d4632b0ab89403211151633172fa0151b6eb3e
naresh <tnaresh564@gmail.com>
2021-09-06 12:48:14 +0530

    c

---
+++ 01_varibles.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+// calculate area of rectangle
+
+// int main()
+// {
+// 	int l,b,a;
+// 	printf("enter the length:\n");
+// 	scanf("%d", &l);
+// 	printf("enter the breath:\n");
+// 	scanf("%d", &b);
+// 	a=l*b;
+// 	printf("area of the rectangle=%d", a);
+
+// 	return 0;
+// }
+
+
+//calculate area of circle
+
+// int main()
+// {
+// 	float pi,r,a;
+// 	pi=3.14159;
+// 	r=3;
+// 	a=pi*r*r;
+// 	printf("the area of the circle=%f",a);
+// 	return 0;
+// }
+
+//temperature converter(c to f)
+
+// int main()
+// {
+// 	float c,f;
+// 	printf("Enter the temp in celsius:");
+// 	scanf("%f", &c);
+// 	f=(c*9/5) + 32;
+// 	printf("Temperature in fahrenheit=%.2f",f);
+// }
+
+//step by step evulation of 3*x/y-z+k
+int main()
+{
+int x=2, y=3, z=3, k=1;
+int result = 3 * x / y - z + k;
+//logic
+//3 * 2 / 3 - 3 + 1
+// 6/3 - 3 +1
+// 2-3+1
+// -1+1
+// 0 
+printf("the result =%d",result);
+}
